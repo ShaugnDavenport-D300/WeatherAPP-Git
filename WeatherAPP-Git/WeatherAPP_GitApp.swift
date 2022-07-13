8c70e226d29589d3863b0f31bc98edbface19ae9
Shaugn Davenport <shaugn.davenport@d300.org>
2022-07-13 13:40:38 -0500

    Initial Commit

---
+++ WeatherAPP-Git/WeatherAPP_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPP_GitApp.swift
+//  WeatherAPP-Git
+//
+//  Created by SD on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPP_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
